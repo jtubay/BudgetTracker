05eedc602e161b887f61b6213fbeadca5b909673
Jean Tubay <jean.tubay.92@gmail.com>
2019-12-15 18:57:04 -0500

    added finishing touches

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "/index.html", "/db.js", "/index.js", "/style.css", "/routes/api.js"];
+const FILES_TO_CACHE = ["/", "/index.html", "/db.js", "/index.js", "/style.css"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
